7e3d0ebcfabe0135453bafca6ba33c7c709f0ed5
SuminJN <wjstnals1211@gmail.com>
2025-03-18 21:23:14 +0900

    feat: 나의 나눔 페이지 추가

--- src/components/sharing/GivenSharing.js
+++ src/components/sharing/GivenSharing.js
@@ -1,6 +1,53 @@
+import { Col, Container, Row, Button } from "react-bootstrap";
+import itemData from "../../assets/json/items.json";
+import Card from "react-bootstrap/Card";
+import img from "../../assets/images/items/shirt.jpeg";
+import React, {useState} from "react";
+import {Drawer} from "antd";
+import {useNavigate} from "react-router-dom";
+
 function GivenSharing() {
+    const navigate = useNavigate();
+
+    const [open, setOpen] = useState(false);
+    const showDrawer = () => {
+        setOpen(true);
+    };
+    const onClose = () => {
+        setOpen(false);
+    };
+
     return (
         <>
+            <Container className="my-5 col col-md-8 col-lg-6">
+                <Card>
+                    <Card.Header as="h3" className="text-center">나눔 완료</Card.Header>
+                    {itemData.map((item, idx) => (
+                        <Card.Body key={idx} className="border">
+
+                            <Row>
+                                <Col lg={3} className="mb-3">
+                                    <Card.Img
+                                        className="border rounded"
+                                        variant="top"
+                                        src={img} width={100}
+                                        height={200}/>
+                                </Col>
+                                <Col xs={8} lg={6}>
+                                    <Row>
+                                        <Card.Title className="mb-3 fs-3">{item.title}</Card.Title>
+                                        <Card.Text>
+                                            {item.description}
+                                        </Card.Text>
+                                    </Row>
+                                </Col>
+                                <Col xs={4} lg={3}>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    ))}
+                </Card>
+            </Container>
         </>
     );
 }
--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,10 +1,11 @@
 import Button from "react-bootstrap/Button";
 import {Col, Container, Form, InputGroup, Row} from "react-bootstrap";
-import Card from "react-bootstrap/Card";
 import img from "../assets/images/items/shirt.jpeg";
-
 import itemData from "../assets/json/items.json";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {Card} from 'antd';
+
+const {Meta} = Card;
 
 
 function Items() {
@@ -34,37 +35,36 @@ function Items() {
                 </Row>
 
 
-                <Row  className="mt-5">
+                <Row className="mt-5">
 
                     <Col md={2} className="p-5 mx-4">
                         <p className="fw-bold pb-2">카테고리</p>
                         <Form>
                             {['디지털기기', '생활가전', '가구/인테리어', '생활/주방', '도서', '뷰티/미용', '취미/게임', '티켓/교환권']
                                 .map((category, index) => (
-                                <div key={index} className="mb-3">
-                                    <Form.Check
-                                        type="radio"
-                                        name="category"
-                                        label={category}
-                                    />
+                                    <div key={index} className="mb-3">
+                                        <Form.Check
+                                            type="radio"
+                                            name="category"
+                                            label={category}
+                                        />
 
-                                </div>
-                            ))}
+                                    </div>
+                                ))}
                         </Form>
                     </Col>
 
                     <Col>
-                        <Row xs={1} md={4} className="g-4">
+                        <Row xs={1} sm={2} md={2} lg={4} className="g-4">
                             {itemData.map((item, index) => (
                                 <Col key={index}>
-                                    <Card onClick={() => onClickCard(item.id)}>
-                                        <Card.Img variant="top" src={img} width={100} height={180}/>
-                                        <Card.Body>
-                                            <Card.Title>{item.title}</Card.Title>
-                                            <Card.Text>
-                                                {item.description}
-                                            </Card.Text>
-                                        </Card.Body>
+                                    <Card onClick={() => onClickCard(item.id)}
+                                          hoverable
+                                          cover={<img alt="example"
+                                                      src={img}/>}
+                                    >
+                                        <p className="fs-5">{item.title}</p>
+                                        <p>{item.description}</p>
                                     </Card>
                                 </Col>
                             ))}
