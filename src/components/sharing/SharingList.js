d77724daae2af5905a5228b6548972d89d1c2b36
SuminJN <wjstnals1211@gmail.com>
2025-03-21 12:21:16 +0900

    feat: 나의 나눔 페이지 반응형 추가

--- src/components/sharing/SharingList.js
+++ src/components/sharing/SharingList.js
@@ -16,30 +16,31 @@ function SharingList() {
 
     return (
         <>
-            <Container className="mt-md-0 mt-lg-5 col-md-12 col-lg-10 col-xl-6">
+            <Container className="mt-md-0 mt-lg-5 col-md-10 col-lg-10 col-xl-8">
                 <Card>
                     <Card.Header as="h2" className="text-center py-3">나눔 중인 물건</Card.Header>
                     {mockItems.map((item, idx) => (
                         <Card.Body key={idx} className="border">
 
-                            <Row>
-                                <Col lg={3} className="mb-3">
+                            <Row className="m-3">
+                                <Col xs={0} sm={0} md={3} lg={3} xl={3}>
                                     <Card.Img
                                         className=""
                                         variant="top"
                                         src={item.photo} width={100}
                                         height={200}/>
                                 </Col>
-                                <Col xs={8} lg={6}>
-                                    <Row>
-                                        <Card.Title className="mb-3 fs-4">{item.title}</Card.Title>
+
+                                <Col xs={0} sm={0} md={6} lg={6} xl={6} className="my-3 my-md-0">
+                                    <Card.Title className="mb-3 fs-4">{item.title}</Card.Title>
                                     <Card.Text>
                                         {item.description}
                                     </Card.Text>
-                                    </Row>
                                 </Col>
-                                <Col xs={4} lg={3}>
-                                    <Button variant="outline-primary" onClick={showDrawer}>
+
+                                <Col className="d-grid d-md-flex align-items-md-center justify-content-md-center">
+                                    <Button variant="outline-primary" onClick={showDrawer}
+                                            style={{whiteSpace: "nowrap"}}>
                                         신청자 보기
                                     </Button>
                                 </Col>
