8f68a98a61e15c5ee421f218fa9f8bcde27d83ef
SuminJN <wjstnals1211@gmail.com>
2025-03-23 17:18:19 +0900

    feat: 나눔 신청자 조회 기능 추가

---
+++ src/mocks/fixtures/mockItemHistory.js
@@ -0,0 +1,107 @@
+export const mockItemHistory = [
+    {
+        "id": "0",
+        "itemId": "0",
+        "userId": "01231231231231231",
+    },
+    {
+        "id": "1",
+        "itemId": "0",
+        "userId": "1",
+    },
+    {
+        "id": "2",
+        "itemId": "0",
+        "userId": "2",
+    },
+    {
+        "id": "3",
+        "itemId": "1",
+        "userId": "3",
+    },
+    {
+        "id": "4",
+        "itemId": "1",
+        "userId": "4",
+    },
+    {
+        "id": "5",
+        "itemId": "2",
+        "userId": "5",
+    },
+    {
+        "id": "6",
+        "itemId": "3",
+        "userId": "6",
+    },
+    {
+        "id": "7",
+        "itemId": "3",
+        "userId": "7",
+    },
+    {
+        "id": "8",
+        "itemId": "4",
+        "userId": "8",
+    },
+    {
+        "id": "9",
+        "itemId": "5",
+        "userId": "9",
+    },
+    {
+        "id": "10",
+        "itemId": "6",
+        "userId": "10",
+    },
+    {
+        "id": "11",
+        "itemId": "7",
+        "userId": "11",
+    },
+    {
+        "id": "12",
+        "itemId": "7",
+        "userId": "12",
+    },
+    {
+        "id": "13",
+        "itemId": "8",
+        "userId": "13",
+    },
+    {
+        "id": "14",
+        "itemId": "8",
+        "userId": "14",
+    },
+    {
+        "id": "15",
+        "itemId": "8",
+        "userId": "15",
+    },
+    {
+        "id": "16",
+        "itemId": "9",
+        "userId": "16",
+    },
+    {
+        "id": "17",
+        "itemId": "10",
+        "userId": "17",
+    },
+    {
+        "id": "18",
+        "itemId": "1",
+        "userId": "18",
+    },
+    {
+        "id": "19",
+        "itemId": "2",
+        "userId": "19",
+    },
+    {
+        "id": "20",
+        "itemId": "3",
+        "userId": "20",
+    }
+]
