3098f84a157aca231328872555fce2375df24a20
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-07 19:54:12 +0900

    feat: enhance Cover component with additional user information fields

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -10,16 +10,22 @@ import { useEffect, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { LoginState } from "../../../recoil/LoginState";
 import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import SelectInput from "@mui/material/Select/SelectInput";
 
 function Cover() {
-  const inputRef = useRef(null);
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(LoginState);
   const [userInfo, setUserInfo] = useState({
     userInfo: null,
     name: null,
     department: null,
   });
-  const [nickname, setNickname] = useState(null);
+  const [nickname, setNickname] = useState("");
+  const [introduction, setIntroduction] = useState("");
+  const [mbti, setMbti] = useState("");
+  const [hobby, setHobby] = useState("");
+  const [tag, setTag] = useState("");
 
   // 폼 제출 함수
   const onSubmit = (event) => {
@@ -29,6 +35,10 @@ function Cover() {
       .post("/api/nanuri/auth/signup", {
         uniqueId: userInfo.uniqueId,
         nickname: nickname,
+        introduction: introduction,
+        mbti: mbti,
+        hobby: hobby,
+        tag: tag,
       })
       .then((res) => {
         setIsLoggedIn(true);
@@ -40,7 +50,6 @@ function Cover() {
     axiosInstance.get("/api/user").then((res) => {
       setUserInfo(res.data);
     });
-    inputRef.current.focus();
   }, []);
 
   return (
@@ -64,73 +73,113 @@ function Cover() {
             서비스를 이용하기 위해서는 가입이 필요합니다.
           </MDTypography>
         </MDBox>
-        <MDBox pt={4} pb={3} px={3} mx={5}>
-          <MDBox mb={2}>
-            <MDTypography variant="h6" gutterBottom>
-              학번
-            </MDTypography>
-            <MDTypography variant="subtitle2" gutterBottom fontWeight="regular">
-              {userInfo.uniqueId}
-            </MDTypography>
-          </MDBox>
-          <MDBox mb={2}>
-            <MDTypography variant="h6" gutterBottom>
-              이름
-            </MDTypography>
-            <MDTypography variant="subtitle2" gutterBottom fontWeight="regular">
-              {userInfo.name}
-            </MDTypography>
-          </MDBox>
-          <MDBox mb={2}>
-            <MDTypography variant="h6" gutterBottom>
-              학부
-            </MDTypography>
-            <MDTypography variant="subtitle2" gutterBottom fontWeight="regular">
-              {userInfo.department}
-            </MDTypography>
-          </MDBox>
-          <MDBox component="form" role="form">
-            <MDBox mt={3} mb={1}>
-              <TextField
-                id="nickname"
-                label="닉네임"
-                variant="outlined"
-                fullWidth
-                required
-                inputRef={inputRef}
-                value={nickname}
-                onChange={(event) => {
-                  setNickname(event.target.value);
-                }}
-              />
+
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <MDBox pt={4} pb={3} px={3} mx={5}>
+              <MDBox mb={2}>
+                <MDTypography variant="h6" gutterBottom>
+                  학번
+                </MDTypography>
+                <MDTypography variant="subtitle2" gutterBottom fontWeight="regular">
+                  {userInfo.uniqueId}
+                </MDTypography>
+              </MDBox>
+              <MDBox mb={2}>
+                <MDTypography variant="h6" gutterBottom>
+                  이름
+                </MDTypography>
+                <MDTypography variant="subtitle2" gutterBottom fontWeight="regular">
+                  {userInfo.name}
+                </MDTypography>
+              </MDBox>
+              <MDBox mb={2}>
+                <MDTypography variant="h6" gutterBottom>
+                  학부
+                </MDTypography>
+                <MDTypography variant="subtitle2" gutterBottom fontWeight="regular">
+                  {userInfo.department}
+                </MDTypography>
+              </MDBox>
+              <MDBox mt={3} mb={1}>
+                <TextField
+                  id="nickname"
+                  label="닉네임"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  autoFocus
+                  value={nickname}
+                  onChange={(event) => {
+                    setNickname(event.target.value);
+                  }}
+                />
+              </MDBox>
             </MDBox>
-            <MDBox display="flex" alignItems="center" ml={-1}>
-              <Checkbox />
-              <MDTypography
-                variant="button"
-                fontWeight="regular"
-                color="text"
-                sx={{ cursor: "pointer", userSelect: "none", ml: -1 }}
-              >
-                &nbsp;&nbsp;I agree the&nbsp;
+          </Grid>
+          <Grid item xs={0} sm={0.1}>
+            <Divider orientation="vertical" sx={{ mx: 0 }} />
+          </Grid>
+          <Grid item xs={12} sm={5.6}>
+            <MDBox pt={3} px={6} textAlign="center">
+              <MDTypography variant="h6" gutterBottom>
+                자신을 소개해 보세요!
               </MDTypography>
-              <MDTypography
-                component="a"
-                href="#"
-                variant="button"
-                fontWeight="bold"
-                color="info"
-                textGradient
-              >
-                Terms and Conditions
-              </MDTypography>
+              <MDBox mt={4} mb={3}>
+                <TextField
+                  id="introduction"
+                  label="소개말"
+                  variant="outlined"
+                  fullWidth
+                  value={introduction}
+                  onChange={(event) => {
+                    setIntroduction(event.target.value);
+                  }}
+                />
+              </MDBox>
+              <MDBox mb={3}>
+                <TextField
+                  id="mbti"
+                  label="MBTI"
+                  variant="outlined"
+                  fullWidth
+                  value={mbti}
+                  onChange={(event) => {
+                    setMbti(event.target.value);
+                  }}
+                />
+              </MDBox>
+              <MDBox mb={3}>
+                <TextField
+                  id="hobby"
+                  label="취미"
+                  variant="outlined"
+                  fullWidth
+                  value={hobby}
+                  onChange={(event) => {
+                    setHobby(event.target.value);
+                  }}
+                />
+              </MDBox>
+              <MDBox mb={3}>
+                <TextField
+                  id="tag"
+                  label="관심 태그"
+                  variant="outlined"
+                  fullWidth
+                  value={tag}
+                  onChange={(event) => {
+                    setTag(event.target.value);
+                  }}
+                />
+              </MDBox>
             </MDBox>
-            <MDBox mt={4} mb={1}>
-              <MDButton variant="gradient" color="info" fullWidth onClick={onSubmit}>
-                회원가입
-              </MDButton>
-            </MDBox>
-          </MDBox>
+          </Grid>
+        </Grid>
+        <MDBox mt={4} mb={1}>
+          <MDButton variant="gradient" color="info" fullWidth onClick={onSubmit}>
+            회원가입
+          </MDButton>
         </MDBox>
       </Card>
     </CoverLayout>
