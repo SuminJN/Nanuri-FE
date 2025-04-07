e67bcdb840cae7c8aef88bb754b817daa70c53e6
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-07 19:54:22 +0900

    feat: add tag data for various product categories

---
+++ src/assets/tagData.js
@@ -0,0 +1,17 @@
+export const tagData = {
+  MAJOR_BOOK: "전공 서적",
+  GENERAL_BOOK: "도서",
+  DIGITAL_DEVICE: "디지털기기",
+  STATIONERY: "문구류",
+  SPORTS: "운동용품",
+  DAILY_SUPPLIES: "생활용품",
+  FOOD: "식료품",
+  MAN_CLOTHES: "남성 의류",
+  WOMAN_CLOTHES: "여성 의류",
+  BEAUTY: "화장품",
+  TICKET_COUPON: "티켓",
+  PET_SUPPLIES: "반려동물 용품",
+  HOBBY_GOODS: "취미 용품",
+  FURNITURE: "가구",
+  ETC: "기타",
+};
