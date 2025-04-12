e056b3dce539e125bb8404002e52243d0efd1c74
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-13 00:10:46 +0900

    feat: ItemDetail component 변경

--- src/layouts/itemDetail/ItemDetail.js
+++ src/layouts/itemDetail/ItemDetail.js
@@ -8,7 +8,7 @@ import axiosInstance from "../../apis/axios";
 import { useNavigate, useParams } from "react-router-dom";
 import MDTypography from "../../components/MDTypography";
 import MDButton from "../../components/MDButton";
-import { Image } from "antd";
+import { Carousel, Image } from "antd";
 import IconButton from "@mui/material/IconButton";
 import { navbarIconButton } from "../../examples/Navbars/DashboardNavbar/styles";
 import Icon from "@mui/material/Icon";
@@ -71,58 +71,73 @@ function ItemDetail() {
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox mt={2} mb={3}>
-        <Grid container spacing={3} mb={2}>
-          <Grid item xs={12} sm={12} md={8}>
-            <Card>
-              <MDBox p={2}>
-                <IconButton
-                  size="small"
-                  disableRipple
-                  sx={navbarIconButton}
-                  variant="contained"
-                  onClick={() => {
-                    navigate(-1);
-                  }}
-                >
-                  <Icon>arrow_back_ios_icon</Icon>
-                </IconButton>
+        <Card>
+          <MDBox px={2} pt={3}>
+            <IconButton
+              size="small"
+              disableRipple
+              sx={navbarIconButton}
+              variant="contained"
+              onClick={() => {
+                navigate(-1);
+              }}
+            >
+              <Icon>arrow_back_ios_icon</Icon>
+            </IconButton>
+          </MDBox>
+          <Grid container spacing={2} sx={{ p: { xs: 2, sm: 3, md: 5 } }}>
+            <Grid item xs={12} sm={12} md={6}>
+              <MDBox>
+                <Carousel arrows infinite={false}>
+                  {item &&
+                    item.images.map((image, index) => (
+                      <div key={index}>
+                        <Image
+                          src={image}
+                          alt="image"
+                          width="100%"
+                          height="400px"
+                          style={{ borderRadius: "8px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)" }}
+                        />
+                      </div>
+                    ))}
+                </Carousel>
               </MDBox>
-              <MDBox p={4}>
-                <Grid container spacing={1}>
-                  <Grid item xs={12}>
-                    <MDBox mb={3} display="flex" alignItems="center">
-                      <MDBox p={1}>
-                        <MDAvatar src={image} alt="something here" shadow="md" size="md" />
-                      </MDBox>
-                      <MDTypography variant="h6" opacity="60%">
-                        {item.nickname}
-                      </MDTypography>
-                    </MDBox>
-                    <MDBox mb={2}>
-                      <MDTypography variant="h4" color="info">
-                        {item.title}
-                      </MDTypography>
-                    </MDBox>
-                    <MDBox mb={8}>
-                      <MDTypography variant="h6" opacity="60%">
-                        {item.category} · {item.createdTime}
-                      </MDTypography>
-                    </MDBox>
-                    <MDBox mb={2} height="200px">
-                      <MDTypography variant="h6">{item.description}</MDTypography>
-                    </MDBox>
-                  </Grid>
-                  <Grid container display="flex" justifyContent="end">
-                    <Grid item xs={12} sm={4}>
-                      <MDBox>
-                        <MDTypography variant="overline">
-                          신청 0 · 관심 {item.wishCount ? item.wishCount : 0} · 조회{" "}
-                          {item.viewCount}
-                        </MDTypography>
-                      </MDBox>
-                      {item.isOwner ? (
-                        <>
-                          <MDBox mb={1}>
+              <MDBox mb={3} display="flex" alignItems="center">
+                <MDBox p={1}>
+                  <MDAvatar src={image} alt="something here" shadow="md" size="md" />
+                </MDBox>
+                <MDTypography variant="h6" opacity="60%">
+                  {item.nickname}
+                </MDTypography>
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <MDBox>
+                <MDBox mb={1}>
+                  <MDTypography variant="h4" color="info">
+                    {item.title}
+                  </MDTypography>
+                </MDBox>
+                <MDBox mb={5}>
+                  <MDTypography variant="h6" opacity="60%">
+                    {item.category} · {item.createdTime}
+                  </MDTypography>
+                </MDBox>
+                <MDBox mb={2}>
+                  <MDTypography variant="h6">{item.description}</MDTypography>
+                </MDBox>
+                <MDBox>
+                  <MDTypography variant="overline">
+                    신청 0 · 관심 {item.wishCount ? item.wishCount : 0} · 조회 {item.viewCount}
+                  </MDTypography>
+                </MDBox>
+                <MDBox>
+                  {item.isOwner ? (
+                    <>
+                      <Grid container spacing={1}>
+                        <Grid item xs={12} sm={6}>
+                          <MDBox>
                             <MDButton
                               variant="outlined"
                               color="info"
@@ -134,6 +149,8 @@ function ItemDetail() {
                               </MDTypography>
                             </MDButton>
                           </MDBox>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
                           <MDBox>
                             <MDButton
                               variant="outlined"
@@ -146,10 +163,14 @@ function ItemDetail() {
                               </MDTypography>
                             </MDButton>
                           </MDBox>
-                        </>
-                      ) : (
-                        <>
-                          <MDBox mb={1}>
+                        </Grid>
+                      </Grid>
+                    </>
+                  ) : (
+                    <>
+                      <Grid container spacing={1}>
+                        <Grid item xs={12} sm={6}>
+                          <MDBox>
                             <MDButton
                               variant="outlined"
                               color="info"
@@ -161,6 +182,8 @@ function ItemDetail() {
                               </MDTypography>
                             </MDButton>
                           </MDBox>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
                           <MDBox>
                             <MDButton
                               variant="outlined"
@@ -173,30 +196,15 @@ function ItemDetail() {
                               </MDTypography>
                             </MDButton>
                           </MDBox>
-                        </>
-                      )}
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </MDBox>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            {item &&
-              item.images.map((image, index) => (
-                <MDBox key={index} mb={2}>
-                  <Image
-                    src={image}
-                    alt="image"
-                    loading="lazy"
-                    width="100%"
-                    height="300px"
-                    style={{ borderRadius: "8px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)" }}
-                  />
+                        </Grid>
+                      </Grid>
+                    </>
+                  )}
                 </MDBox>
-              ))}
+              </MDBox>
+            </Grid>
           </Grid>
-        </Grid>
+        </Card>
       </MDBox>
     </DashboardLayout>
   );
