1674ec41291f99bf0981c64b0500f00d79745f3a
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-15 23:03:27 +0900

    feat: 채팅 기능 임시 추가

--- src/layouts/chat/ChatRoom.js
+++ src/layouts/chat/ChatRoom.js
@@ -1,197 +1,198 @@
-import React, { useCallback, useRef, useState, useEffect } from "react";
-import testImg from "../../assets/images/team-2.jpg";
+import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import SockJS from "sockjs-client";
 
-import * as StompJs from "@stomp/stompjs";
-
-// heroicons
-import { CameraIcon, ChevronLeftIcon, MegaphoneIcon } from "@heroicons/react/24/outline";
-import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
 import { useRecoilValue } from "recoil";
-import { UserId } from "../../recoil/UserId";
-import { Stomp } from "@stomp/stompjs";
+import { NicknameState } from "../../recoil/NicknameState";
+import { Stomp, Client } from "@stomp/stompjs";
+import Grid from "@mui/material/Grid";
+import DashboardNavbar from "../../examples/Navbars/DashboardNavbar";
+import DashboardLayout from "../../examples/LayoutContainers/DashboardLayout";
+import MDBox from "../../components/MDBox";
+import axiosInstance from "../../apis/axios";
 
-export default function ChatRoom() {
-  let navigate = useNavigate();
-  const userId = useRecoilValue(UserId);
-
-  const socket = new SockJS("http://localhost:8080/ws-stomp");
-  const stompClient = Stomp.over(socket);
+function ChatRoom() {
+  const navigate = useNavigate();
   const { roomId } = useParams();
 
-  let [client, changeClient] = useState(null);
-  const [chat, setChat] = useState(""); // 입력된 chat을 받을 변수
-  const [chatList, setChatList] = useState([]); // 채팅 기록
+  const SERVER_URL = "http://localhost:8080/ws-stomp"; // STOMP 연결 엔드포인트
+  const PUB_ENDPOINT = "/pub/chat/message"; // 메시지를 전송하기 위한 엔드포인트
+  const SUB_ENDPOINT = `/sub/chat/room/${roomId}`; // 메시지를 수신하기 위한 엔드포인트
 
-  //컴포넌트가 변경될 때 객체가 유지되어야하므로 'ref'로 저장
+  // STOMP가 연결되고 생성한 Client를 관리하는 상태 관리
+  const [wsClient, setWsClient] = useState();
 
-  // 내가 보낸 메시지, 받은 메시지에 각각의 스타일을 지정해 주기 위함
-  const msgBox = chatList.map((item, idx) => {
-    if (Number(item.sender) !== userId) {
-      return (
-        <div key={idx}>
-          <div>
-            <img src={testImg} alt="" />
-          </div>
-          <div>
-            <span>{item.data}</span>
-          </div>
-          <span>{item.date}</span>
-        </div>
-      );
-    } else {
-      return (
-        <div key={idx}>
-          <div>
-            <span>{item.data}</span>
-          </div>
-          <span>{item.date}</span>
-        </div>
-      );
-    }
+  // 채팅에서 누적되는 데이터를 관리합니다.
+  const [messages, setMessages] = useState([]);
+
+  // 사용자의 구분을 짓기 위해 임시로 발급한 사용자 아이디
+  const nickname = useRecoilValue(NicknameState);
+
+  // 채팅에서 보내지는 데이터를 관리합니다.
+  const [messageObj, setMessageObj] = useState({
+    message: "",
+    roomId: roomId,
+    nickname: nickname,
   });
 
-  // const connect = () => {
-  //   // 소켓 연결
-  //   try {
-  //     const clientdata = new StompJs.Client({
-  //       brokerURL: "ws://localhost:8080/chat",
-  //       connectHeaders: {
-  //         login: "",
-  //         passcode: "password",
-  //       },
-  //       debug: function (str) {
-  //         console.log(str);
-  //       },
-  //       reconnectDelay: 5000, // 자동 재 연결
-  //       heartbeatIncoming: 4000,
-  //       heartbeatOutgoing: 4000,
-  //     });
-  //
-  //     // 구독
-  //     clientdata.onConnect = function () {
-  //       clientdata.subscribe("/sub/channels/" + roomId, callback);
-  //     };
-  //
-  //     clientdata.activate(); // 클라이언트 활성화
-  //     changeClient(clientdata); // 클라이언트 갱신
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  /**
+   * STOMP를 연결하고 라이프사이클을 관리하는 Handler
+   */
+  const stompHandler = (() => {
+    return {
+      connect: () => {
+        // [STEP1] 연결 시 Client 객체를 생성합니다.
+        const client = new Client({
+          webSocketFactory: () => new SockJS(SERVER_URL),
+          reconnectDelay: 5000,
+          heartbeatIncoming: 4000,
+          heartbeatOutgoing: 4000,
 
-  const connect = () => {
-    // 연결
-    stompClient.connect({}, () => {
-      // 구독 (수신할 채팅방 ID)
-      stompClient.subscribe(`/sub/chat/room/${roomId}`, (message) => {
-        console.log("수신:", JSON.parse(message.body));
-      });
-    });
-  };
+          // [STEP2] 웹 소켓 연결
+          onConnect: (conn) => {
+            console.log("[+] WebSocket 연결이 되었습니다.", conn);
+            // [WebSocket - Subscribe] 특정 엔드포인트로 메시지를 수신합니다.
+            client.subscribe(SUB_ENDPOINT, (message) => {
+              console.log("수신:", JSON.parse(message.body));
+              const receiveData = JSON.parse(message.body);
+              setMessages((prevMessages) => [
+                ...prevMessages,
+                {
+                  message: receiveData.message,
+                  senderNickname: receiveData.senderNickname,
+                  roomId: receiveData.roomId,
+                  receiverNickname: receiveData.receiverNickname,
+                },
+              ]);
+            });
+          },
+          // 웹 소켓 연결 종료
+          onWebSocketClose: (close) => {
+            console.log("[-] WebSocket 연결이 종료 되었습니다.", close);
+          },
+          // 웹 소켓 연결 에러
+          onWebSocketError: (error) => {
+            console.error("[-] 웹 소켓 연결 오류가 발생하였습니다.", error);
+          },
+          // STOMP 프로토콜 에러
+          onStompError: (frame) => {
+            console.error("Broker reported error: " + frame.headers["message"]);
+            console.error("Additional details: " + frame.body);
+          },
+        });
+        setWsClient(client); // 구성한 Client 객체를 상태 관리에 추가합니다.
+        client.activate(); // Client를 활성화 합니다.
 
-  const disConnect = () => {
-    // client.deactivate();
-    stompClient.disconnect();
-  };
+        return () => {
+          stompHandler.disconnect(); // Socket 연결을 종료합니다.
+        };
+      },
+      /**
+       * 웹 소켓 메시지를 전송합니다.
+       */
+      sendMessage: () => {
+        if (wsClient && wsClient.connected && messageObj.message.trim() !== "") {
+          // [WebSocket - Publish] 특정 엔드포인트로 메시지를 전송합니다.
+          console.log(messageObj);
+          wsClient.publish({
+            destination: PUB_ENDPOINT,
+            body: JSON.stringify({ ...messageObj }),
+          });
+          // 입력한 값을 초기화합니다.
+          setMessageObj({ ...messageObj, message: "" });
+        }
+      },
+      /**
+       * 웹 소켓 연결을 종료합니다.
+       */
+      disconnect: () => {
+        console.log("[-] 웹 소켓 연결을 종료합니다.");
+        if (wsClient) {
+          wsClient.deactivate();
+          setWsClient(undefined);
+        }
+      },
+    };
+  })();
 
-  // 콜백함수 => ChatList 저장하기
-  const callback = function (message) {
-    if (message.body) {
-      let msg = JSON.parse(message.body);
-      setChatList((chats) => [...chats, msg]);
+  const getPreviousChat = async () => {
+    const response = await axiosInstance.get(`/api/chat/room/${roomId}/messages`);
+    if (response.status === 200) {
+      console.log(response.data);
+      setMessages(response.data);
+    } else {
+      console.log("error");
     }
-  };
-
-  const sendChat = () => {
-    if (chat === "") {
-      return;
-    }
-
-    // client.publish({
-    //   destination: "/pub/chat/" + roomId,
-    //   body: JSON.stringify({
-    //     type: "",
-    //     sender: userId,
-    //     channelId: "1",
-    //     data: chat,
-    //   }),
-    // });
-
-    // 메시지 전송 (보낼 채팅 메시지)
-    stompClient.send(
-      "/pub/chat/message",
-      {},
-      JSON.stringify({
-        roomId: roomId,
-        senderId: userId,
-        receiverId: "21900636",
-        message: chat,
-      })
-    );
-
-    setChat("");
   };
 
   useEffect(() => {
-    // 최초 렌더링 시 , 웹소켓에 연결
-    // 우리는 사용자가 방에 입장하자마자 연결 시켜주어야 하기 때문에,,
-    connect();
+    getPreviousChat().then((r) => {
+      stompHandler.connect();
+    });
 
-    return () => disConnect();
+    return () => {
+      stompHandler.disconnect();
+    };
   }, []);
 
-  const onChangeChat = (e) => {
-    setChat(e.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
   return (
-    <>
-      {/* {JSON.stringify(user)} */}
-      {/* <GlobalStyle/> */}
-      {/*<div>*/}
-      {/*  /!* 상단 네비게이션 *!/*/}
-      {/*  <div>*/}
-      {/*    <ChevronLeftIcon*/}
-      {/*      onClick={() => {*/}
-      {/*        navigate("/chatlist ");*/}
-      {/*      }}*/}
-      {/*    />*/}
-      {/*    <span>상대방 이름</span>*/}
-      {/*    <MegaphoneIcon onClick={() => navigate(`/report/1`)} />*/}
-      {/*  </div>*/}
-
-      {/*  /!* 채팅 리스트 *!/*/}
-      {/*  <div>{msgBox}</div>*/}
-
-      {/*  /!* 하단 입력폼 *!/*/}
-      {/*  <form onSubmit={handleSubmit}>*/}
-      {/*    /!* <input type="file" accept='image/*'/>  *!/*/}
-      {/*    <CameraIcon />*/}
-      {/*    <div>*/}
-      {/*      <div>*/}
-      {/*        <input*/}
-      {/*          type="text"*/}
-      {/*          id="msg"*/}
-      {/*          value={chat}*/}
-      {/*          placeholder="메시지 보내기"*/}
-      {/*          onChange={onChangeChat}*/}
-      {/*          onKeyDown={(ev) => {*/}
-      {/*            if (ev.keyCode === 13) {*/}
-      {/*              sendChat();*/}
-      {/*            }*/}
-      {/*          }}*/}
-      {/*        />*/}
-      {/*      </div>*/}
-      {/*      <ArrowUpCircleIcon value="전송" onClick={sendChat} />*/}
-      {/*    </div>*/}
-      {/*  </form>*/}
-      {/*</div>*/}
-    </>
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox mt={2} mb={3}>
+        <Grid container justifyContent="center">
+          <Grid item xs={12} md={6}>
+            <div
+              style={{
+                flex: 1,
+                flexDirection: "column",
+                justifyContent: "center",
+                alignContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              <h1>STOMP을 이용한 채팅방입니다. </h1>
+              <div style={{ flexDirection: "row" }}>
+                <input
+                  type="text"
+                  value={messageObj.message}
+                  onChange={(e) => setMessageObj({ ...messageObj, message: e.target.value })}
+                  onKeyDown={(e) => {
+                    if (e.keyCode === 13) {
+                      stompHandler.sendMessage();
+                    }
+                  }}
+                />
+                <button onClick={stompHandler.sendMessage}>전송</button>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  height: 300,
+                  backgroundColor: "#f5d682",
+                  border: "1px solid red",
+                  margin: 20,
+                }}
+              >
+                {messages.map((m, index) => (
+                  <h1
+                    style={{
+                      fontSize: 13,
+                      textAlign: "left",
+                    }}
+                    key={`messages-${index}`}
+                  >
+                    {m.senderNickname === nickname ? `[ME] ${m.message}` : `[OTHER] ${m.message}`}
+                  </h1>
+                ))}
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </MDBox>
+    </DashboardLayout>
   );
 }
+
+export default ChatRoom;
