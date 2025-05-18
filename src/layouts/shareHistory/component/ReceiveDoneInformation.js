e0d326a347352524cd21af42b706011e08d60be6
Sumin Jeon <wjstnals1211@gmail.com>
2025-05-18 14:36:16 +0900

    feat: add nickname display to ItemCard in ReceiveDoneInformation component

--- src/layouts/shareHistory/component/ReceiveDoneInformation.js
+++ src/layouts/shareHistory/component/ReceiveDoneInformation.js
@@ -32,6 +32,7 @@ function ReceiveDoneInformation() {
                   <Grid item xs={12} sm={6} md={6} lg={4} xl={3} key={idx}>
                     <ItemCard
                       itemId={item.itemId}
+                      nickname={item.nickname}
                       title={item.title}
                       description={item.description}
                       category={item.category}
