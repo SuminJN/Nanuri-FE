a2076876baed0f6acb3b263b66ddf5f1d5323b60
Sumin Jeon <wjstnals1211@gmail.com>
2025-05-22 22:59:48 +0900

    feat: update background image and add navigation button to home in Basic component

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -3,9 +3,11 @@ import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import MDButton from "components/MDButton";
 import BasicLayout from "layouts/authentication/components/BasicLayout";
-import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+import bgImage from "assets/images/hgu.png";
+import { useNavigate } from "react-router-dom";
 
 function Basic() {
+  const navigate = useNavigate();
   const handleSignInClick = () => {
     window.location.href = process.env.REACT_APP_HISNET_LOGIN_URL;
   };
@@ -31,9 +33,21 @@ function Basic() {
         <MDBox pt={4} pb={3} px={3}>
           <MDBox component="form" role="form">
             <MDBox mb={1}>
-              <MDButton variant="outlined" color="info" fullWidth onClick={handleSignInClick}>
-                히즈넷 로그인
-              </MDButton>
+              <MDBox mb={2}>
+                <MDButton variant="outlined" color="info" fullWidth onClick={handleSignInClick}>
+                  히즈넷 로그인
+                </MDButton>
+              </MDBox>
+              <MDBox>
+                <MDButton
+                  variant="outlined"
+                  color="dark"
+                  fullWidth
+                  onClick={() => navigate("/home")}
+                >
+                  홈으로 이동
+                </MDButton>
+              </MDBox>
             </MDBox>
           </MDBox>
         </MDBox>
