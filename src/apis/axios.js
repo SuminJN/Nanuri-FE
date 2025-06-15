5ebd633e0f7aaa713d51a481b3aceb84babe5465
Sumin Jeon <wjstnals1211@gmail.com>
2025-06-15 21:33:52 +0900

    fix: Remove duplicate error alert for conflict status (409) in axios.js

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -35,9 +35,6 @@ axiosInstance.interceptors.response.use(
     } else if (status === 404) {
       window.alert("요청한 리소스를 찾을 수 없습니다.");
       return Promise.reject(error);
-    } else if (status === 409) {
-      window.alert("이미 존재하는 데이터입니다. 다른 값을 입력해주세요.");
-      return Promise.reject(error);
     } else if (status === 500) {
       window.alert("서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
       return Promise.reject(error);
